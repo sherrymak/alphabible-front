39217cf42f27ae3877d3b3363f9c48389dec1b2f
sherrymak <janenjerishera@gmail.com>
2026-03-26 11:06:59 +0300

    Add files via upload

---
+++ services/api.js
@@ -0,0 +1,28 @@
+const BASE_URL = process.env.REACT_APP_API_URL;
+
+export const getWeekByNumber = async (weekNumber) => {
+  const res = await fetch(`${BASE_URL}/weeks/${weekNumber}`);
+  return res.json();
+};
+
+export const getChapterById = async (bookOrder, chapterNumber) => {
+  const res = await fetch(
+    `${BASE_URL}/biblechapters/${bookOrder}/${chapterNumber}`
+  );
+  return res.json();
+};
+
+export const getNextChapter = async (bookOrder, chapterNumber) => {
+  const res = await fetch(
+    `${BASE_URL}/biblechapters/${bookOrder}/${chapterNumber}/next`
+  );
+  return res.json();
+};
+
+export const getPreviousChapter = async (bookOrder, chapterNumber) => {
+  const res = await fetch(
+    `${BASE_URL}/biblechapters/${bookOrder}/${chapterNumber}/previous`
+  );
+  return res.json();
+};
+
